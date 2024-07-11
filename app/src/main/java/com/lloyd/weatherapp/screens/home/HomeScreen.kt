3154f8c5eed55ad58dd3bcabf139e59e3b942be8
LloydDoctor <lloyd.consultant@metapos.co.za>
2024-07-11 16:36:38 +0200

    Location coordinates

--- app/src/main/java/com/lloyd/weatherapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/lloyd/weatherapp/screens/home/HomeScreen.kt
@@ -1,7 +1,18 @@
 package com.lloyd.weatherapp.screens.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.lloyd.weatherapp.screens.DeviceViewModel
+import com.lloyd.weatherapp.utils.ConnectivityViewModel
 
-class HomeScreen(navController: NavController) {
+@Composable
+fun HomeScreen(navController: NavController) {
+    val connectivityViewModel = hiltViewModel<ConnectivityViewModel>()
+//    val deviceViewModel = hiltViewModel<DeviceViewModel>()
+    LaunchedEffect(key1 = connectivityViewModel.appConnectivityStatus(),
+        block ={
 
+        })
 }
